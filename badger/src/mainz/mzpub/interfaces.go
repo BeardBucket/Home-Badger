4df4876dc8d09e9507a6237221fa339e2818d585
Paulson McIntyre <paul@gpmidi.net>
2023-04-10 16:08:00 -0400

    Had retry arg in wrong interface f

--- badger/src/mainz/mzpub/interfaces.go
+++ badger/src/mainz/mzpub/interfaces.go
@@ -16,7 +16,7 @@ type Main interface {
 	Vpr() *viper.Viper
 	FailIt(msg string, err error)
 	L() *logrus.Logger
-	CreateCache(retry bool) (*cache.Cache[string], error)
+	CreateCache() (*cache.Cache[string], error)
 	GetCache(retry bool) (*cache.Cache[string], error)
 	CacheGet(ctx context.Context, key string) (string, error)
 	CacheSet(ctx context.Context, key string, value string, options ...store.Option) error
